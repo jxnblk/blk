531a4f4463c73054818359545a47bad218739184
Brent Jackson <jacksonblack@gmail.com>
2014-12-04 22:45:14 -0500

    Stats and core package

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,13 @@ var minifyCss = require('gulp-minify-css');
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 var webserver = require('gulp-webserver');
+var cssstats = require('gulp-css-statistics');
+var handlebars = require('gulp-compile-handlebars');
 
 gulp.task('css', function() {
-  gulp.src('./src/css/base.css')
+  gulp.src('./src/css/*.css')
     .pipe(basswork())
+    .pipe(gulp.dest('./css'))
     .pipe(minifyCss())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./css'));
@@ -23,12 +26,28 @@ gulp.task('js', function() {
     .pipe(gulp.dest('./js'));
 });
 
+gulp.task('stats', function() {
+  gulp.src('./css/*.css')
+    .pipe(cssstats())
+    .pipe(gulp.dest('./stats'));
+});
+
+gulp.task('build', function() {
+  var model = {};
+  model.blk = require('./stats/base.json');
+  model.blkCore = require('./stats/base-core.json');
+  gulp.src('./views/**/*.hbs')
+    .pipe(handlebars(model))
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest('.'));
+});
+
 gulp.task('serve', function() {
   gulp.src('.')
     .pipe(webserver({}));
 });
 
-gulp.task('default', ['css', 'js', 'serve'], function() {
-  gulp.watch(['./src/**/*'], ['css', 'js']);
+gulp.task('default', ['css', 'js', 'serve', 'stats', 'build'], function() {
+  gulp.watch(['./src/**/*', './views/**/*'], ['css', 'js', 'stats', 'build']);
 });
 
