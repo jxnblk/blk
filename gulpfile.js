ba1d9e9b6464f0a4a1b16ee9f1ecc8418eb24462
Brent Jackson <jacksonblack@gmail.com>
2014-12-05 18:57:47 -0500

    More custom styles

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var cssstats = require('gulp-css-statistics');
 var handlebars = require('gulp-compile-handlebars');
 var s3 = require('gulp-s3');
 var gzip = require('gulp-gzip');
+var Humanize = require('humanize-plus');
 
 gulp.task('css', function() {
   gulp.src('./src/css/*.css')
@@ -39,10 +40,16 @@ gulp.task('stats', function() {
 
 gulp.task('build', function() {
   var model = {};
-  model.blk = require('./stats/base.json');
-  model.blkCore = require('./stats/base-core.json');
+  model.blk = require('./stats/blk.json');
+  model.blkCore = require('./stats/blk-core.json');
   gulp.src('./views/**/*.hbs')
-    .pipe(handlebars(model))
+    .pipe(handlebars(model, {
+      helpers: {
+        filesize: function(string) {
+          return Humanize.fileSize(string);
+        }
+      }
+    }))
     .pipe(rename({ extname: '.html' }))
     .pipe(gulp.dest('.'));
 });
