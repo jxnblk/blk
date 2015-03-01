24061092b8d5817a4f12e795275576834e38612f
Brent Jackson <jacksonblack@gmail.com>
2015-03-01 12:40:37 -0500

    Update to Basscss v5

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var _ = require('lodash');
 var geomicons = require('geomicons-open');
 var cheerio = require('cheerio');
 
+//var Autobass = require('autobass');
+
 gulp.task('css', function() {
   gulp.src('./src/*.css')
     .pipe(basswork())
@@ -42,12 +44,26 @@ gulp.task('build', function() {
     return $.html();
   };
   data.colors = [
+    'black',
+    'navy',
     'blue',
-    'red',
-    'yellow',
+    'aqua',
+    'teal',
+    'olive',
     'green',
+    'lime',
+    'yellow',
+    'orange',
+    'red',
+    'maroon',
+    'fuchsia',
+    'purple',
   ];
   data.cdn = '//d2v52k3cl9vedd.cloudfront.net/blk/' + data.version + '/blk.min.css';
+
+  // Development
+  //data.cdn = '/css/blk.css';
+
   data.stats = require('./stats/blk.json');
   data.number = function(n) {
     return Humanize.formatNumber(n);
@@ -56,6 +72,19 @@ gulp.task('build', function() {
     return Humanize.filesize(n);
   };
   data.coreStats = require('./stats/blk-core.json');
+  var typographySectionTpl = _.template(fs.readFileSync('./partials/typography-section.html', 'utf8'));
+  data.partials = {
+    typographySection: function(locals) {
+      // color, background, dark, reverse
+      _.assign({
+        color: 'black',
+        background: false,
+        dark: false,
+        reverse: false
+      }, locals);
+      return typographySectionTpl(locals);
+    },
+  };
   var compile = function() {
     return through.obj(function(file, encoding, callback) {
       var src = file.contents.toString();
